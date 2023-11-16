437857c1304aa129589cdc5986b26c1cd0701302
Leen27 <m13840918032@163.com>
2023-11-16 08:00:40 +0800

    feat: zixian class

--- packages/zixian/lib/main.ts
+++ packages/zixian/lib/main.ts
@@ -1,45 +1,25 @@
 import * as PIXI from "pixi.js";
 
-export const createPlayer = (domId: string | HTMLElement) => {
-  const dom: HTMLElement | null = typeof domId == 'string' ? document.body.querySelector(domId) : domId;
-
-  if (!dom) return;
-
-  const app = new PIXI.Application({ background: "#1099bb", resizeTo: dom });
-
-  dom.appendChild(app.view as any);
-
-  const container = new PIXI.Container();
-
-  app.stage.addChild(container);
-
-  // Create a new texture
-  const texture = PIXI.Texture.from("https://pixijs.com/assets/bunny.png");
-
-  // Create a 5x5 grid of bunnies
-  for (let i = 0; i < 25; i++) {
-    const bunny = new PIXI.Sprite(texture);
-
-    bunny.anchor.set(0.5);
-    bunny.x = (i % 5) * 40;
-    bunny.y = Math.floor(i / 5) * 40;
-    container.addChild(bunny);
+class Zixian {
+  _domContainer: HTMLElement
+  _render: PIXI.Application
+  constructor(dom: HTMLElement) {
+    this._createRender(dom)
   }
 
-  // Move container to the center
-  container.x = app.screen.width / 2;
-  container.y = app.screen.height / 2;
+  _createRender(dom: HTMLElement) {
+    const app = new PIXI.Application({ background: "#1099bb", resizeTo: dom });
+    dom.appendChild(app.view as any);
+    const container = new PIXI.Container();
+    app.stage.addChild(container);
+    this._render = app
+  }
+}
 
-  // Center bunny sprite in local container coordinates
-  container.pivot.x = container.width / 2;
-  container.pivot.y = container.height / 2;
-
-  // Listen for animate update
-  app.ticker.add((delta) => {
-    // rotate the container!
-    // use delta to create frame-independent transform
-    container.rotation -= 0.01 * delta;
-  });
+export const createPlayer = (domId: string | HTMLElement) => {
+  const dom: HTMLElement | null = typeof domId == 'string' ? document.body.querySelector(domId) : domId;
+  if (!dom) return;
+  return new Zixian(dom)
 };
 
 export const zixian = () => {}
