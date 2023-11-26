aab2127ee807c1c0b31cf2942833060a9651691a
Leen27 <m13840918032@163.com>
2023-11-26 20:22:39 +0800

    fix

--- packages/zixian/types/gui/object-info.d.ts
+++
@@ -1,6 +0,0 @@
-import { Panel } from '../gui';
-export declare const createObjectInfoPanel: ({ width, height, pixelSizeX, }: {
-    pixelSizeX: number;
-    width: number;
-    height: number;
-}) => Panel;
